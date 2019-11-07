58432a43df7ce6d4d1827c13f7e39460f5e9fc4b
陈骁杰 <xiaojie@gymbo-online.com>
2019-11-07 15:26:41 +0800

    增加测试信息

---
+++ dist/index.js
@@ -0,0 +1,87 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const canvas_1 = require("canvas");
+const emoji = require("node-emoji");
+const fs = require("fs");
+const path = require("path");
+class CanvasEmoji {
+    constructor(ctx) {
+        this.canvasCtx = ctx;
+    }
+    getEmojiKeys(str) {
+        const emojiArr = [];
+        emoji.replace(str, (item) => {
+            emojiArr.push(`${item.key}`);
+        });
+        return emojiArr;
+    }
+    replaceEmojiToEmojiName(str) {
+        const emojiArr = [];
+        str = emoji.replace(str, (item) => {
+            emojiArr.push(`{${item.key}}`);
+            return `{${item.key}}`;
+        });
+        return {
+            str,
+            emojiArr,
+        };
+    }
+    drawPngReplaceEmoji(data) {
+        const { canvasCtx } = this;
+        const { fillStyle, font, y, emojiW, emojiH, } = data;
+        let { text, x, length } = data;
+        canvasCtx.fillStyle = fillStyle;
+        canvasCtx.font = font;
+        const emojiArr = [];
+        text = emoji.replace(text, (item) => {
+            emojiArr.push(`{${item.key}}`);
+            return `{${item.key}}`;
+        });
+        let ctxText;
+        for (const emojiItem of emojiArr) {
+            const index = text.indexOf(emojiItem);
+            if (length !== -1 && length - text.substring(0, index).length <= 0) {
+                canvasCtx.fillText(`${text.substring(0, length)}...`, x, y);
+                ctxText = this.canvasCtx.measureText(`${text.substring(0, length)}...`);
+                x += ctxText.width;
+                break;
+            }
+            canvasCtx.fillText(text.substring(0, index), x, y);
+            ctxText = canvasCtx.measureText(text.substring(0, index));
+            x += ctxText.width;
+            const emojiImg = new canvas_1.Image();
+            emojiImg.src = fs.readFileSync(path.join(__dirname, `../emoji_pngs/${emojiItem.replace('{', '').replace('}', '')}.png`));
+            canvasCtx.drawImage(emojiImg, x, y - (emojiH / 2), emojiW, emojiH);
+            x += 36;
+            text = text.substr(index + emojiItem.length);
+            if (length !== -1) {
+                length -= (text.substring(0, index).length + 1);
+                if (length === 0) {
+                    canvasCtx.fillText('...', x, 1300);
+                    ctxText = canvasCtx.measureText('...');
+                    x += ctxText.width;
+                    break;
+                }
+            }
+        }
+        if (emojiArr.length === 0) {
+            if (length) {
+                text = this.showText(text, length);
+            }
+            canvasCtx.fillText(text, x, y);
+            const ctxText = canvasCtx.measureText(text);
+            x += ctxText.width;
+        }
+        return { x };
+    }
+    showText(text, length = 10) {
+        if (text.length > length) {
+            return text.slice(0, length) + '...';
+        }
+        else {
+            return text;
+        }
+    }
+}
+exports.CanvasEmoji = CanvasEmoji;
+//# sourceMappingURL=index.js.map
---
+++ test/test.js
@@ -0,0 +1,26 @@
+const { createCanvas } = require('canvas');
+const { CanvasEmoji } = require('../dist/index');
+const fs  = require('fs');
+
+function test() {
+  const canvas = createCanvas(800, 200);
+  const canvasCtx = canvas.getContext('2d');
+  const canvasEmoji = new CanvasEmoji(canvasCtx);
+  const a = canvasEmoji.drawPngReplaceEmoji({
+    text: '测试一下哦💋💃测试一下💋测试一下💋💃测试一下💋测试一下💋💃',
+    fillStyle: '#000000',
+    font: 'bold 36px Impact',
+    x: 0,
+    y: 100,
+    emojiW: 36,
+    emojiH: 36,
+    length: 10,
+  })
+  const out = fs.createWriteStream(__dirname + '/test.png');
+  const stream = canvas.createPNGStream();
+  stream.pipe(out);
+  out.on('finish', () => console.log('The PNG file was created.'));
+  return a;
+}
+
+console.log(test());
